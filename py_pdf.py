30b380b957bb646be384f3927325ed44961d3d08
begieian <begieianrivera@gmail.com>
2023-01-09 17:21:22 +0800

    pushing all

---
+++ py_pdf.py
@@ -0,0 +1,25 @@
+import PyPDF2
+import sys
+
+inputs = sys.argv[1:]
+
+def pdf_merger(pdf_list):
+    merger = PyPDF2.PdfMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+
+pdf_merger(inputs)
+
+
+# with open('dummy.pdf', 'rb') as file:
+#     reader = PyPDF2.PdfReader(file)
+#     page = reader.pages[0]
+#     page.rotate(270)
+#     writer = PyPDF2.PdfWriter()
+#     writer.add_page(page)
+#     with open('tilt.pdf', 'wb') as new_file:
+#         writer.write(new_file)
+
