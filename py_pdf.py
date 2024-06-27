027e238abd820cd678e9238cf832cb1deb2f030e
Begie Ian Rivera <41794650+begieian@users.noreply.github.com>
2024-06-27 11:00:36 +0800

    Update py_pdf.py

--- py_pdf.py
+++ py_pdf.py
@@ -5,7 +5,7 @@ inputs = sys.argv[1:]
 
 def pdf_merger(pdf_list):
     merger = PyPDF2.PdfMerger()
-    for pdf in pdf_list:
+    for pdf in pdf_list
         print(pdf)
         merger.append(pdf)
     merger.write('super.pdf')
